f1fa916402e915c7733db56296e8eff030c649fe
Daniel <u22099dandev@gmail.com>
2024-11-18 17:44:55 +0000

    update

--- server.js
+++ server.js
@@ -4,10 +4,28 @@ const { Server } = require("socket.io");
 
 const app = express();
 const httpServer = createServer(app);
-const io = new Server(httpServer, { /* options */ });
-
-io.on("connection", (socket) => {
-  // ...
+const PORT = process.env.PORT || 3000;
+const io = new Server(httpServer, {
+  cors: {
+    origin: "https://zephyr-ktqp.onrender.com", //Update to your frontend origin
+  },
 });
 
-httpServer.listen(3000);
+io.on("connection", (socket) => {
+  console.log("a user connected");
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+  socket.on("chat message", (msg) => {
+    console.log("message: " + msg);
+    io.emit("chat message", msg); //Broadcasts to all connected clients
+  });
+  socket.on("join", (room) => {
+    socket.join(room);
+    console.log("user joined room", room);
+  });
+});
+
+httpServer.listen(PORT, () => {
+  console.log("listening on Port: "+PORT);
+});
