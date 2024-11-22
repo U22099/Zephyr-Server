dea8e66a8e0dc116d23a508d7e4af926cdbe9c10
U22099 <nifemiolaniyi4@gmail.com>
2024-11-22 19:04:04 +0100

    update server.js

--- server.js
+++ server.js
@@ -3,6 +3,14 @@ const { createServer } = require("http");
 const { Server } = require("socket.io");
 
 const app = express();
+app.use((req, res, next) => {
+  console.log(`${req.method}\t${req.url.split("?")[0]}`);
+  next();
+});
+app.get('/ping', (req, res) => {
+  const currentTime = new Date();
+  res.json({ currentTime: currentTime.toISOString() });
+});
 const httpServer = createServer(app);
 const PORT = process.env.PORT || 3000;
 const io = new Server(httpServer, {
